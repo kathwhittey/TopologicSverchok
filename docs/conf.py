4db8ba0be7d05ef665370b00a928e789a2c3ce62
SeaFan <kewhittey@gmail.com>
2022-08-22 10:48:22 +0100

    remove the import vertext etc

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../nodes/Topologic'))
+sys.path.insert(0, os.path.abspath('..'))
 
-import VertexByCoordinates
  # this is the part I'm struggling with
 
 
