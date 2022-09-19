4e48c3410d0ffa546eed10023d3b574588d9ec80
SeaFan <kewhittey@gmail.com>
2022-09-19 16:25:51 +0100

    Update conf.py
    typo?

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ autodoc_mock_imports = ["topologic","ifcopenshell", "bpy", "faulthandler"]
 # ones.
 extensions = ['sphinx.ext.autodoc']
 
-#document functionio.open(), reading its signature and docstring fromthe source file
+#document function io.open(), reading its signature and docstring fromthe source file
 .. autofunction:: io.open
 
 #document whole classes or even modules automatically
